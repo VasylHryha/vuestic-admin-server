f5105ce73461ca6c84e25220c08094efc6878aa2
Vasyl <vasyl.hryha@epicmax.co>
2024-12-26 11:20:34 +0200

    - chore: cleanup

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userRoutes = require('./users');
 const rootRoutes = require('./root');
-const checkoutSession = require('./checkout-sesssion')
 const stripeApi = require('./stripe');
 
 router.use('/users', userRoutes);
 router.use('/', rootRoutes);
-router.use('/api/checkout', checkoutSession);
 router.use('/api', stripeApi);
 
 
